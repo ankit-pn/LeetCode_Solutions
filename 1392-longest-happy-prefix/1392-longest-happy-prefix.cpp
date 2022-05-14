0189dee45e7bb7b32878ea75963550ca05179c5d
Ankit <61223051+ankit-pn@users.noreply.github.com>
2022-05-14 10:46:36 +0530

    Time: 847 ms (5.15%), Space: 20.2 MB (39.12%) - LeetHub

---
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string longestPrefix(string s) {
+         int n=s.length();
+        vector<int> dp(n+1,0);
+        int i=0,j=1;
+        while(j<n && i<n){
+            if(s[i]==s[j]){
+                dp[j]=i+1;
+                i++;
+                j++;
+            }
+            else{
+                if(i!=0)
+                    i=dp[i-1];
+                else
+                    j++;
+            }    
+        }
+        for(auto it:dp)
+            cout<<it<<" ";
+        return s.substr(0,dp[n-1]);
+    }
+};
