1ffcb66b7871d4d40c8c75d985f8cae3b8c3a447
Ankit <61223051+ankit-pn@users.noreply.github.com>
2022-05-14 17:27:05 +0530

    Time: 34 ms (44.02%), Space: 16.6 MB (30.88%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
         int n=s.length();
+        string t=s+s;
+        t=t.substr(1,2*n-2);
         if(n==1)
             return 0;
         int i=0,j=1;
@@ -19,19 +21,42 @@ public:
                     j++;
             }
         }
-        int x=*max_element(ls.begin(),ls.end());
+        // int x=*max_element(ls.begin(),ls.end());
+        // int c=0;
+        // for(auto it:ls)
+        //     cout<<it<<" ";
+        // for(int i=0;i<n;i++){
+        //     if(ls[i]==x)
+        //         c++;
+        // }
+        // int len=n-x;
+        // string s1=s.substr(0,len);
+        // string s2=s.substr(n-len,len);
+        // if(x==0)
+        //     return 0;
+        // return (s1==s2);
+        i=0,j=-1;
         int c=0;
-        for(auto it:ls)
-            cout<<it<<" ";
-        for(int i=0;i<n;i++){
-            if(ls[i]==x)
+        cout<<t;
+        while(i<t.length()){
+            if(t[i]==s[j+1]){
                 c++;
+                if(c==s.length()){
+                    return 1;
+                }
+                i++;
+                j++;
+            }
+            else{
+                if(j>=0){
+                    j=ls[j]-1;    
+                }
+                else{
+                    i++;
+                }
+                c=j+1;
+            }
         }
-        int len=n-x;
-        string s1=s.substr(0,len);
-        string s2=s.substr(n-len,len);
-        if(x==0)
-            return 0;
-        return (s1==s2);
+        return false;
     }
 };
