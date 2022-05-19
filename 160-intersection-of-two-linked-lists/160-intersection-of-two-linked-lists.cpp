7ac4f0fc2bc67cc7e58b65e4caf0bc8a84d42efb
Ankit <61223051+ankit-pn@users.noreply.github.com>
2022-05-19 10:38:49 +0530

    Time: 49 ms (80.52%), Space: 14.6 MB (32.76%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -9,19 +9,50 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* temp1;
-        ListNode* temp2;
-        ListNode* temp3=headB;
-        while(temp3){
-            temp2=temp3;
-            temp1=headA;
-       while(temp1){
-           if(temp2==temp1){
-               return temp2;
-           }
-              temp1=temp1->next;
+       //  ListNode* temp1;
+       //  ListNode* temp2;
+       //  ListNode* temp3=headB;
+       //  while(temp3){
+       //      temp2=temp3;
+       //      temp1=headA;
+       // while(temp1){
+       //     if(temp2==temp1){
+       //         return temp2;
+       //     }
+       //        temp1=temp1->next;
+       //  }
+       //      temp3=temp3->next;
+       //  }
+       //  return NULL;
+        
+        // there are two optimal soln present, length and that of two pointer;
+        
+        ListNode* temp1=headA,*temp2=headB;
+        int n=0,m=0;
+        while(temp1){
+            temp1=temp1->next;
+            n++;
         }
-            temp3=temp3->next;
+        while(temp2){
+            temp2=temp2->next;
+            m++;
+        }
+        int diff=abs(n-m);
+        while(diff){
+            if(n>m){
+                headA=headA->next;
+                diff--;
+            }
+            else {
+                headB=headB->next;
+                diff--;
+            }
+        }
+        while(headA!=NULL && headB!=NULL){
+            if(headA==headB)
+                return headA;
+            headA=headA->next;
+            headB=headB->next;
         }
         return NULL;
     }
