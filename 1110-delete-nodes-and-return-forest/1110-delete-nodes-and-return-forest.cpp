426a2ffda85a2e44ab78209fd113a9d5a7646ed7
Ankit <61223051+ankit-pn@users.noreply.github.com>
2022-05-28 17:56:16 +0530

    Time: 32 ms (52.74%), Space: 25.4 MB (61.12%) - LeetHub

---
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    set<int> st;
+    vector<TreeNode* > res;
+    // bool f=1;
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        for(auto it:to_delete)
+            st.insert(it);
+        dfs(root,true);
+        return res;
+    }
+    TreeNode* dfs(TreeNode* root,bool f){
+        if(!root)
+            return root;
+            bool x= st.find(root->val)!=st.end();
+        if(f && !x){
+            res.push_back(root);
+        }
+         // f=x;
+        root->left=dfs(root->left,x);
+        root->right=dfs(root->right,x);
+        if(x)
+            return NULL;
+        else
+            return root;
+    }
+};
