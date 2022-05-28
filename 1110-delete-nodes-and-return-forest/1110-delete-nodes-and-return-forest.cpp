a3659c6e72d7312c5973f1cbc69a50f9d3ba83a8
Ankit <61223051+ankit-pn@users.noreply.github.com>
2022-05-28 18:12:07 +0530

    Time: 26 ms (74.98%), Space: 25.3 MB (61.12%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -17,22 +17,40 @@ public:
     vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
         for(auto it:to_delete)
             st.insert(it);
-        dfs(root,true);
+        dfs(root);
+        if(root)
+        res.push_back(root);
         return res;
     }
-    TreeNode* dfs(TreeNode* root,bool f){
+//     TreeNode* dfs(TreeNode* root,bool f){
+//         // if(!root)
+//         //     return root;
+//         //     bool x= st.find(root->val)!=st.end();
+//         // if(f && !x){
+//         //     res.push_back(root);
+//         // }
+//         //  // f=x;
+//         // root->left=dfs(root->left,x);
+//         // root->right=dfs(root->right,x);
+//         // if(x)
+//         //     return NULL;
+//         // else
+//         //     return root;
+        
+        
+//     }
+    void dfs(TreeNode* &root){
         if(!root)
-            return root;
-            bool x= st.find(root->val)!=st.end();
-        if(f && !x){
-            res.push_back(root);
+            return ;
+        dfs(root->left);
+        dfs(root->right);
+        if(st.find(root->val)!=st.end()){
+            if(root->left)
+            res.push_back(root->left);
+            if(root->right)
+            res.push_back(root->right);
+            root=NULL;
+            delete root;
         }
-         // f=x;
-        root->left=dfs(root->left,x);
-        root->right=dfs(root->right,x);
-        if(x)
-            return NULL;
-        else
-            return root;
     }
 };
