d335b95391fb770e22f8dfeb1354c3f3532b6239
Ankit <61223051+ankit-pn@users.noreply.github.com>
2022-06-28 14:30:24 +0530

    Time: 70 ms (26.66%), Space: 31 MB (25.10%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        if(k==1){
+            vector<vector<int>> ans;
+            for(int i=1;i<=n;i++)
+                ans.push_back({i});
+            return ans;
+        }
+        if(n==k){
+            vector<vector<int>> ans;
+            vector<int> temp;
+            for(int i=1;i<=n;i++)
+                temp.push_back(i);
+            ans.push_back(temp);
+            return ans;
+        }
+        // vector<vector<int>> ans;
+        auto x=combine(n-1,k-1);
+        for(int j=0;j<x.size();j++){
+            x[j].push_back(n);
+        }
+        auto p=combine(n-1,k);
+        for(auto &it:p){
+            x.push_back(it);
+        }
+        return x;
+        
+    }
+    // if this got accepted , mf you can do anthing.
+    
+    
+    
+    
+    // vector<int> dfs(){
+    //     if(k==1 || n==0 || )
+    //     auto temp=dfs(n-1,k-1);
+    //     temp.push_back(n);
+    //     return temp;
+    // }
+};
