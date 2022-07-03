3db79750c7fd34d283a275936eba1d0a51c426c5
Ankit <61223051+ankit-pn@users.noreply.github.com>
2022-07-03 07:50:09 +0530

    Time: 106 ms (99.60%), Space: 66.2 MB (92.51%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.cpp
+++ 904-fruit-into-baskets/904-fruit-into-baskets.cpp
@@ -8,6 +8,7 @@ public:
             return 2;
         int c2=0;
         int c1=arr[0];
+        // finding 2nd element 
         for(int i=0;i<n;i++){
             if(arr[i]!=c1){
                 c2=arr[i];
@@ -18,16 +19,24 @@ public:
         int ans=0;
         int i=1;
         int curr=0;
+        // rq is the index of previos one 
+        // ex - 1 2 1 1 2 2 2 2 3
+        // since when we start from 3 all we need is to maintain index of 2 ie from 2 2 2 2 3 wla index. thats all we need.
         int rq=0;
         while(i<n){
+            // curr is used to find the index of position from where 2 starts again, i,e unequal part.
+            // 
             while(i<n && (arr[i]==c1 || arr[i]==c2)){
                 if(arr[i]!=arr[i-1])
                     curr=i;
                     i++;
             }
             // cout<<i<<rq<<"\n";
+            // then we will find requ ans, and max it.
             ans=max(i-rq,ans);
+            // update the rq index
             rq=curr;
+            // update c1 and c2 for next search
             if(i<n)
             c2=arr[i];
             if(curr<n)
