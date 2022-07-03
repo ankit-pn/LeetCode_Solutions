cd8ad20a6ff550b94042c9c5aa3e5344213999f5
Ankit <61223051+ankit-pn@users.noreply.github.com>
2022-07-03 18:39:27 +0530

    Time: 84 ms (31.36%), Space: 7.9 MB (47.67%) - LeetHub

--- 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
@@ -13,6 +13,17 @@ public:
         int ans=n+1;
         for(int j=0;j<n;j++){
             mp[s[j]]--;
+            // well this condn simply implies all of them should have count less than a particular value, if not then we will simple increse right side pointer value to include
+            // and when count of all value <=k, like some of them may have count==0 and some of them also have count=k.
+            // or atleast one of them will have. well then if we chage all elemnt of that window , then there is grantee that we will get stirng of equal char.
+            // becz if we change all the chracter of that string
+            // we will put the requid char that we reuired to make string equal to what we want.
+            // when all of them are euqla then there will be decrement happed.
+            // but then this is gonna be i=j+1.
+            // and we will have min ans=0.
+            // we start srinking left side of window.
+            // incrementing what we have decremented.
+            // thanks.
             while(i<n && mp['Q']<=k && mp['W']<=k && mp['E']<=k && mp['R']<=k){
                 mp[s[i]]++;
                 ans=min(ans,j-i+1);
