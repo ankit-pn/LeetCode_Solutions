7bd6fa87682a5d7106fa35c36855307a548de21b
Ankit <61223051+ankit-pn@users.noreply.github.com>
2022-07-07 21:44:31 +0530

    Time: 165 ms (11.94%), Space: 42 MB (53.91%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -3,6 +3,8 @@ public:
     int numRescueBoats(vector<int>& arr, int limit) {
         sort(arr.begin(),arr.end());
         int c=0,n=arr.size(),i=0,j=n-1;
+        // well you have to use two pointer , not implement anything without proper thinking you foolish , because at end of day it is gonna wrong , they why to waste time on random things.
+        //periods.
         while(i<=j){
             if(arr[i]+arr[j]<=limit){
                 c++;
