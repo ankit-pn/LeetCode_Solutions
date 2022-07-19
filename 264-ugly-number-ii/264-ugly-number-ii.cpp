c2c47b3a647583ff9e34a54f25b5b7c3adb99bc9
Ankit <61223051+ankit-pn@users.noreply.github.com>
2022-07-19 12:44:14 +0530

    Time: 13 ms (68.73%), Space: 7.5 MB (80.26%) - LeetHub

---
+++ 264-ugly-number-ii/264-ugly-number-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        // u are fucking blushit not able to write even a shittest code .
+        
+        // priority_queue<int,vector<int>,greater<int>> pq;
+        // pq.push(1);
+        // int c=0;
+        // while(pq.size()<=n){
+        //     int x=pq.top();
+        //     pq.push(2*x);
+        //     pq.push(3*x);
+        //     pq.push(5*x);
+        //     pq.pop();
+        // }
+        // while(c!=n){
+        //     ans=pq.top();
+        //     pq.pop();
+        //     c++;
+        // }
+        // return ans;
+      // wait for writing dp soln you need to maintain t1,t2,t3 three variables.
+        
+        
+        // lemme start now.
+        
+        
+        int t1=1,t2=1,t3=1;
+        vector<int> dp(n+1,0);
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=min({2*dp[t1],3*dp[t2],5*dp[t3]});
+            if(dp[i]==2*dp[t1])t1++;
+             if(dp[i]==3*dp[t2])t2++;
+             if(dp[i]==5*dp[t3])t3++;
+        }
+        return dp[n];
+    }
+};
